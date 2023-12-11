603f9ff194c447de8a5a818e31989b82838ae937
allen <294931812@qq.com>
2023-12-11 14:30:19 +0800

    wip: add code

--- craco.config.js
+++ craco.config.js
@@ -10,6 +10,22 @@ module.exports = {
         ...webpackConfig.resolve.alias,
         '@': path.join(__dirname, './src/electron-renderer'),
       }
+
+      const tsLoaderRule = webpackConfig.module.rules.find(
+        (rule) => rule.oneOf && rule.oneOf.some((loader) => loader.loader && loader.loader.includes('ts-loader'))
+      )
+
+      if (tsLoaderRule) {
+        tsLoaderRule.oneOf.forEach((loader) => {
+          if (loader.loader && loader.loader.includes('ts-loader')) {
+            loader.options = {
+              ...loader.options,
+              configFile: path.join(__dirname, './src/electron-renderer/tsconfig.json'),
+            }
+          }
+        })
+      }
+
       return webpackConfig
     },
   },
